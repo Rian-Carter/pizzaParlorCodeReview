6f38ab2b67fb6ffd728b2e745a72d2b3c7fe0368
Rian Carter <R.Carter86@hotmail.com>
2022-02-25 12:43:55 -0800

    add pizza prototype to calculate cost based on size and number of toppings

--- js/scripts.js
+++ js/scripts.js
@@ -14,3 +14,13 @@ function Customer(name) {
 Pizza.prototype.countToppings = function() {
   this.numberOfToppings = this.toppings.length;
 }
+
+Pizza.prototype.onePizzaCost = function() {
+  if (this.size === "small") {
+    this.cost = 10 + this.numberOfToppings;
+  } else if (this.size === "medium") {
+    this.cost = 20 + this.numberOfToppings;
+  } else {
+    this.cost = 30 + this.numberOfToppings;
+  }
+}
