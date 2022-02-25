5aba92b8c727948afba8c4d769f8a9b50ddc3d60
Rian Carter <R.Carter86@hotmail.com>
2022-02-25 15:01:08 -0800

    starting over, taking a new approach

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,14 @@
+function Customer(name) {
+  this.name = name;
+  this.order = [];
+  this.orderCost = 0;
+}
+
 function Pizza(size, toppings) {
   this.size = size;
   this.toppings = toppings;
   this.numberOfToppings;
   this.cost;
-}
-
-function Customer(name) {
-  this.name = name;
-  this.order = [];
-  this.orderCost = 0;
 }
 
 Pizza.prototype.countToppings = function() {
@@ -24,28 +24,4 @@ Pizza.prototype.onePizzaCost = function() {
     this.cost = 30 + this.numberOfToppings;
   }
 }
-
-$(document).ready(function() {
-  $("#pizzaFormSubmit").click(function(event) {
-    event.preventDefault();
-    let nameInput = $("#customerNameInput").val();
-    let sizeInput = $("#pizzaSizeInput").val();
-    let toppingsInput = [];
-    $("#pizzaToppings input:checkbox[name=toppings]:checked").each(function() {
-      toppingsInput.push($(this).val())
-    });
-
-    let onePizza = new Pizza(sizeInput, toppingsInput);
-    let customerOne = new Customer(nameInput);
-    customerOne.order.push(onePizza);
-
-    $(".output").show()
-    $(".outName").text(customerOne.name);
-    for (let i = 0; i < customerOne.order.length; i++) {
-      let i;
-      $(".outOrder").append("One " + customerOne.order[i].size + " pizza with " + customerOne.order[i].numberOfToppings + " toppings." + '<br>');
-    }
-    $(".outputTotal").text(customerOne.orderCost);
-  });
-});
 
