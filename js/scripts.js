3475ce4fbe4bdd91b3a07d1bf16e82cd96b8d845
Rian Carter <R.Carter86@hotmail.com>
2022-02-25 21:47:31 -0800

    fix typo....

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,21 @@
+
+$(document).ready(function() {
+  $("form#orderPizza").submit(function(event) {
+    event.preventDefault();
+    let customerName = $('.customerName').val();
+    let pizzaSize = $('input:radio[name=pizzaSize]:checked').val();
+    let toppings = [];
+    $('input:checkbox[name=toppings]:checked').each(function() {
+      const toppingList = $(this).val();
+      toppings.push(toppingList)
+    })
+    let pizzaPie = new Pizza(pizzaSize, toppings);
+    let resultHold = pizzaPie.calculate();
+    $('.name').text("Hello, " + customerName + ", your order will be ready shortly!");
+    $('.total').text("$" + resultHold + " is your total");
+  })
+});
+
 function Pizza(size, toppings) {
   this.size = size;
   this.toppings = toppings;
@@ -7,31 +25,13 @@ Pizza.prototype.calculate = function() {
   let pizzaTotal = 0;
   if (this.size === "small") {
     pizzaTotal += 10;
-    console.log(pizzaTotal)
   } else if (this.size === "medium") {
     pizzaTotal += 15;
   } else if (this.size === "large") {
     pizzaTotal += 20;
+  } else if (this.size === "colossal") {
+    pizzaTotal += 25;
   }
   pizzaTotal += this.toppings.length * 1;
   return pizzaTotal;
 };
-
-$(document).ready(function() {
-  $("form#orderPizza").submit(function(event) {
-    event.preventDefault();
-    let customerName = $('.customerName').val();
-    console.log(customerName);
-    let pizzaSize = $('input:radio[name=pizzaSize]:checked').val();
-    console.log(pizzaSize);
-    let topping = [];
-    $('input:checkbox[name=toppings]:checked').each(function() {
-      const toppingList = $(this).val();
-      toppings.push(toppingList)
-    })
-    console.log(topping);
-    let pizzaPie = new Pizza(pizzaSize, toppings);
-    console.log(pizzaPie);
-    $('.name').text("Hello, ' + customerName + ', your order will be ready shortly!");
-  })
-});
